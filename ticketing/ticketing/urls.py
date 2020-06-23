96bada3756e5edb26aa380bf5b068a55c40df092
Max Ilin <max.makasinn@gmail.com>
2020-06-23 15:35:55 +0300

    rename project name

--- ticketing/ticketing/urls.py
+++ ticketing/ticketing/urls.py
@@ -1,4 +1,4 @@
-"""tickering URL Configuration
+"""ticketing URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/3.0/topics/http/urls/
