50f5547b73bb46fd3ecf2f3f746591e37298e978
Max Ilyin <max.makasinn@gmail.com>
2020-06-25 22:02:54 +0300

    create submit and ticket views, create submit page

--- ticketing/ticketing/views.py
+++ ticketing/ticketing/views.py
@@ -3,3 +3,11 @@ from django.shortcuts import render
 
 def index(request):
     return render(request, "index.html")
+
+
+def submit(request):
+    return render(request, "submit.html")
+
+
+def tickets(request):
+    return render(request, "tickets.html")
