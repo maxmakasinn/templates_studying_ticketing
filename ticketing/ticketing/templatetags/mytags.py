2a4446d4dc7679777ae8030d4880719054c4d893
Max Ilyin <max.makasinn@gmail.com>
2020-07-11 21:00:40 +0300

    create oun tag with date-time tag

--- ticketing/ticketing/templatetags/mytags.py
+++ ticketing/ticketing/templatetags/mytags.py
@@ -3,10 +3,13 @@ from django import template
 register = template.Library()
 
 time_format = "%A, %B, %d, %Y at %I:%M%p"
+time_format_mil = "%A, %B, %d, %Y at %H:%M%p"
 
 
-def my_date(old_value):
-    return old_value
+def my_date(old_value, miltime=False):
+    if miltime:
+        return old_value.strftime(time_format_mil)
+    return old_value.strftime(time_format)
 
 
 register.filter("my_date", my_date)
